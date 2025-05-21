48fa85e3addeb1a941db507fd4e0bd8b51dbd7b4
rm94362 <rm94362@estudante.fieb.edu.br>
2025-05-21 11:25:49 +0000

    flex

--- src/PokeFlex.jsx
+++ src/PokeFlex.jsx
@@ -1,15 +1,17 @@
  import poke from './assets/pokeapi_256.png'
 import eevee from './assets/eevee.jpg' 
 import './Home.css'
-import PokeGrid from './PokeGrid'
-import PokeFlex from './PokeFlex'
+
 
 const Home = () => {
 
     return (
-        <div>
+        <div className="poke-flex">
         <img src={poke} />
        <img src={eevee} />
+        <img src={poke} />
+         <img src={poke} />
+          <img src={poke} />
         </div>
     
     )
--- src/PokeFlexGrid.jsx
+++ src/PokeFlexGrid.jsx
@@ -1,15 +1,17 @@
  import poke from './assets/pokeapi_256.png'
 import eevee from './assets/eevee.jpg' 
-import './Home.css'
-import PokeGrid from './PokeGrid'
-import PokeFlex from './PokeFlex'
+import './PokeGrid.css'
 
-const Home = () => {
+
+const PokeGrid = () => {
 
     return (
         <div>
         <img src={poke} />
        <img src={eevee} />
+        <img src={poke} />
+         <img src={poke} />
+          <img src={poke} />
         </div>
     
     )
